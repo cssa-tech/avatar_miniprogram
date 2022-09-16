74f6f6a5fd78f660d5e652c20b3065f639955311
gong208 <60023638+gong208@users.noreply.github.com>
2022-09-15 23:25:54 -0500

    switch year

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,23 +1,7 @@
 // pages/select/select.js
 const app = getApp();
 
-const _tousList = [
-  {
-    coverImgUrl: '../../images/uiuc_mid_autumn_22/wang.png',
-    author: '王若玘',
-  },
-  {
-    coverImgUrl: '../../images/uiuc_mid_autumn_22/chen.png',
-    author: '陈谦路',
-  }, 
-  {
-    coverImgUrl: '../../images/uiuc_mid_autumn_22/du1.png',
-    author: '林子秋',
-  }, 
-  {
-    coverImgUrl: '../../images/uiuc_mid_autumn_22/du2.png',
-    author: '林子秋',
-  },
+const _tousList1 = [
   {
     coverImgUrl: '../../images/uiuc_mid_autumn_21/pekkie.png',
     author: 'Pekkie',
@@ -36,16 +20,49 @@ const _tousList = [
   },
 ]
 
+const _tousList2 = [{
+  coverImgUrl: '../../images/uiuc_mid_autumn_22/wang.png',
+  author: '王若玘',
+},
+{
+  coverImgUrl: '../../images/uiuc_mid_autumn_22/chen.png',
+  author: '陈谦路',
+}, 
+{
+  coverImgUrl: '../../images/uiuc_mid_autumn_22/du1.png',
+  author: '林子秋',
+}, 
+{
+  coverImgUrl: '../../images/uiuc_mid_autumn_22/du2.png',
+  author: '林子秋',
+},
+]
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    touList: _tousList,
+    year: 2022,
+    touList: _tousList2,
     indicatorDots: true
   },
 
+  switchTo2021() {
+    this.setData ({
+      touList: _tousList1,
+      year: 2021
+    })
+  },
+
+  switchTo2022() {
+    this.setData ({
+      touList: _tousList2,
+      year: 2022
+    })
+  },
+  
   /**
    * 生命周期函数--监听页面加载
    */
@@ -57,9 +74,16 @@ Page({
   detail: function (e) {
     // console.log(e)
     var touNum = e.currentTarget.dataset.tounum
-    if (_tousList[touNum] != undefined) {
-      app.globalData.toubgsrc = _tousList[touNum].coverImgUrl
-      wx.navigateTo({ url: '../madeph/madeph' })
+    if (this.data.year == 2021) {
+      if (_tousList1[touNum] != undefined) {
+        app.globalData.toubgsrc = _tousList1[touNum].coverImgUrl
+        wx.navigateTo({ url: '../madeph/madeph' })
+      }
+    } else if (this.data.year == 2022) {
+      if (_tousList2[touNum] != undefined) {
+        app.globalData.toubgsrc = _tousList2[touNum].coverImgUrl
+        wx.navigateTo({ url: '../madeph/madeph' })
+      }
     }
   },
 
